b67aa8028623a79bbbc8056c78bfffe47fbff760
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-09 14:36:01 -0700

    Add pseudocode, create HTML file for testing/debugging, and turn string into array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Turn string into array
+    let stringArray = Array.from(string);
+    // Reverse array
+    // Join array back together
 };
 
 // Do not edit below this line
