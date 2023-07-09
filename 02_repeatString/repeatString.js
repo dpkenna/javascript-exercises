2a30084204e1da34897d1529ad0f9852c7360deb
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-09 14:17:20 -0700

    Solve the tests and unskip them

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        repString = ''
+        for (i = 1; i <= num; i++) {
+            repString += string;
+        }
+        return repString;
+    } else {
+        return 'ERROR'
+    }
 };
 
 // Do not edit below this line
