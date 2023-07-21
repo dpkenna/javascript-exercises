0dba31ff067786baf4d2f863f65de48bce8abaa8
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-21 15:42:52 -0700

    Set up pseudocode

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeValue) {
+    // Loop through the array
+    // If value not found, ignore
+    // Get the index of the array
+    // Remove the value
 };
 
 // Do not edit below this line
