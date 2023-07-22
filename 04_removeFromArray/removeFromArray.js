fbadf1cdd6da1d02053be1bf2071c87f035d0ffd
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-22 14:15:35 -0700

    Add debugging statements

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,12 @@
 const removeFromArray = function(array, ...removeValues) {
     // Loop through the values to remove
     for (value in removeValues) {
+        console.log(value);
         // Loop through the array
         for (item in array) {
             // Get the index of the array
             index = array.indexOf(value);
+            console.log(index);
             // If value not found, ignore
             if (index == -1) {
                 return;
