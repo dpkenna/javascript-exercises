ca42925b979fb885290dcf5f0a439af7813cad74
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-23 13:18:55 -0700

    Remove debugging statement and uncomment test module

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,10 @@
 const removeFromArray = function(array, ...removeValues) {
     // Loop through the values to remove
     for (value of removeValues) {
-        console.log("value: " + value);
         // Loop through the array
         for (item of array) {
             // Get the index of the array
             index = array.indexOf(value);
-            console.log("index: " + index);
             // If value not found, ignore
             if (index != -1) {
                 array.splice(index, 1);
@@ -16,7 +14,5 @@ const removeFromArray = function(array, ...removeValues) {
 return array;
 };
 
-console.log(removeFromArray([1,2,3], 3,2));
-
 // Do not edit below this line
-// module.exports = removeFromArray;
+module.exports = removeFromArray;
