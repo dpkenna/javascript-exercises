6adca6f9356b21e6d139a14dbc1a33cf7d6dfe2e
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-23 15:51:40 -0700

    Put the numbers into an array and sort them

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,8 +2,11 @@ const sumAll = function(firstNum, secondNum) {
     // Return "ERROR" if a negative number is given or if a non-number is given
     if (firstNum < 0 || secondNum < 0 || typeof firstNum != "number" || typeof secondNum != "number") {
         return "ERROR";
-    }
+    } else {
     // Arrange the numbers from smallest to largest in case the larger number is given first
+    sortArray = [firstNum, secondNum];
+    sortArray.sort(function(a,b){return a-b});
+    }
     // Loop through the numbers from first to last
     // Add each number to the sum
     // Return the sum
