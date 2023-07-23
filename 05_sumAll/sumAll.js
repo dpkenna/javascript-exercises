8633622a5cf659039fdf175268bbdc3d6f7a9ebb
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-23 15:58:50 -0700

    Loop through the numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,12 +3,16 @@ const sumAll = function(firstNum, secondNum) {
     if (firstNum < 0 || secondNum < 0 || typeof firstNum != "number" || typeof secondNum != "number") {
         return "ERROR";
     } else {
-    // Arrange the numbers from smallest to largest in case the larger number is given first
-    sortArray = [firstNum, secondNum];
-    sortArray.sort(function(a,b){return a-b});
+        // Arrange the numbers from smallest to largest in case the larger number is given first
+        sortArray = [firstNum, secondNum];
+        sortArray.sort(function(a,b){return a-b});
+        // Loop through the numbers from first to last
+        for (let i = sortArray[0]; i <= sortArray[1]; i++){
+        // Add each number to the sum
+        }
     }
-    // Loop through the numbers from first to last
-    // Add each number to the sum
+
+
     // Return the sum
 };
 
