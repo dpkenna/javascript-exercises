48f058481d61e5035865f52a0fcbe7d875eb1017
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-23 15:40:34 -0700

    Set up pseudocode

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    // Return "ERROR" if a negative number is given or if a non-number is given
+    // Arrange the numbers from smallest to largest in case the larger number is given first
+    // Loop through the numbers from first to last
+    // Add each number to the sum
+    // Return the sum
 };
 
 // Do not edit below this line
