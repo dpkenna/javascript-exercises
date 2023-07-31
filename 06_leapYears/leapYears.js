57e130db0935e4a986a09ba92aceb698223d9ac0
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-30 17:23:06 -0700

    Set up pseduocode

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // If the year is not divisible by 100
+        // Determine whether the year is divisible by 4
+    // If the year is divisible by 100
+        // Determine whether the year is divisible by 400
 };
 
 // Do not edit below this line
