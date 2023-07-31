0d57ca7f0eb47f1eccef1c7b85ccaecb1341b9c1
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-30 17:34:05 -0700

    Set up pseudocode

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  // Convert the temperature
+  // Round to nearest single decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // Convert the temperature
+  // Round to nearest single decimal
 };
 
 // Do not edit below this line
