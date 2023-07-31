349ccec9baea9e2a78e99fbda9eb0bf72189b4e3
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-30 17:38:53 -0700

    Round the temperature

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,8 @@
 const convertToCelsius = function(temperature) {
   // Convert the temperature
-  temp = (temperature - 32) / 1.8
+  temp = (temperature - 32) / 1.8;
   // Round to nearest single decimal
+  roundedTemp = temp.toFixed(1);
 };
 
 const convertToFahrenheit = function(temperature) {
