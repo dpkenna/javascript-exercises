ed6bb1bc7199d6a5274ae362f7c38d3031452852
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-07-30 17:27:04 -0700

    Set up the conditionals

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,15 @@
 const leapYears = function(year) {
     // If the year is not divisible by 100
-        // Determine whether the year is divisible by 4
+    // Determine whether the year is divisible by 4
+    if (year % 100 != 0 && year % 4 == 0) {
+        return true;
     // If the year is divisible by 100
-        // Determine whether the year is divisible by 400
+    // Determine whether the year is divisible by 400
+    } else if (year % 100 == 0 && year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
