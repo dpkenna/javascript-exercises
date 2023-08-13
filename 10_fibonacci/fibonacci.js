60f5f8737be26842e43a5b2b08d340c441932d93
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 15:45:16 -0700

    Set up pseudocode

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    // Declare an array
+    // Add each fibonacci number to an array up to place - 1
+    // Return array item at index[place - 1]
 };
 
 // Do not edit below this line
