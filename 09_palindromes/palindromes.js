cdc5c4d34a116d204e295e370812ecab61b21d0d
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 14:45:29 -0700

    Set up pseudocode

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // Convert the string to lowercase
+    // Remove the punctuation and spaces
+    // Reverse the string
+    // Compare the strings
 };
 
 // Do not edit below this line
