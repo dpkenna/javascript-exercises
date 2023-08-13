5a64cb63b2a16c30bbc6b180e5d2a78fa5588420
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 16:08:42 -0700

    Set up the pseudocode

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    // Declare a result array
+    // Loop through the objects in the input array
+    // Add the value of title to the result array
+    // Return the result array
 };
 
 // Do not edit below this line
