cbd0c013914ffb97934e11e1c98bfe924c5479b1
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 15:53:57 -0700

    Loop through up to place, add the numbers together, and return place-1

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,12 @@
 const fibonacci = function(place) {
     // Declare an array
+    let sequence = [1, 1];
     // Add each fibonacci number to an array up to place - 1
+    for (i = 2; i < place; i++) {
+        sequence[i] = sequence[i - 2] + sequence[i - 1];
+    }
     // Return array item at index[place - 1]
+    return sequence[place - 1];
 };
 
 // Do not edit below this line
