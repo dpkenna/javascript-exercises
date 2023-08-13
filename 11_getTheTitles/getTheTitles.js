9ba097895533105cb2dc1119a4cbe56b9b854c08
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 16:16:16 -0700

    Loop through the array and add the title to the result

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -2,7 +2,10 @@ const getTheTitles = function(array) {
     // Declare a result array
     let titles = [];
     // Loop through the objects in the input array
-    // Add the value of title to the result array
+    for (i = 0; i < array.length; i++) {
+        // Add the value of title to the result array
+        titles[i] = array.title;
+    }
     // Return the result array
 };
 
