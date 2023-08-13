fe637f9c5f1fa8e161120353a2c866d0203aab3a
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 14:09:02 -0700

    Set up pseudocode

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	// Add two numbers
+  // Return the sum
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	// Subtract two numbers
+  // Return the difference
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	// Loop through array
+  // Add each number to the sum
+  // Return the sum
 };
 
-const multiply = function() {
-
+const multiply = function(...values) {
+  // Loop through values
+  // Multiply each value
+  // Return the total
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	// Raise the first value to the power of the second
+  // Return the total
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	// If 0 or 1 return 1
+  // Loop through each value
+  // Multiply each value by the next value and add to total
+  // Return the total
 };
 
 // Do not edit below this line
