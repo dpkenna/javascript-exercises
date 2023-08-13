a08452dc306c17f35693748d661dc2c97200114f
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 16:48:29 -0700

    Calculate the age

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -4,9 +4,15 @@ const findTheOldest = function(array) {
     // Loop through the array of objects
     for (person in array) {
         // If there is a death date
+        if (person.yearOfDeath != undefined) {
             // Subtract the birth date from the death date
+            let age = person.yearOfDeath - person.yearOfBirth;
+        } else {
         // Else
             // Subtract the birth date from today's date
+            let date = new Date();
+            let age = date.getFullYear() - person.yearOfBirth;
+        }
         // Add the result to the result array
     }
     // Compare the items in the result array
