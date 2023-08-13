f6f6e8bb08db57fdf0a83483a1828c1105438291
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 15:11:27 -0700

    Remove debug code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -5,11 +5,9 @@ const palindromes = function (string) {
     noSpaceString = lowString.split(' ').join('');
     // Remove the punctuation
     noPuncString = noSpaceString.replace(/[^a-z0-9]/gi, '');
-    console.log(noPuncString);
     // Reverse the string
     revString = noPuncString.split('').reverse().join('');
     // Compare the strings
-    console.log(revString);
     if (noPuncString == revString) {
         return true;
     } else {
@@ -17,7 +15,6 @@ const palindromes = function (string) {
     }
 };
 
-palindromes('Racecar!');
 
 // Do not edit below this line
-// module.exports = palindromes;
+module.exports = palindromes;
