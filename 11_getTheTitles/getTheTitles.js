3c2d9e58c6854bf9bf0834b6b70d2680c3560b67
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 16:09:13 -0700

    Declare the result array

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
 const getTheTitles = function(array) {
     // Declare a result array
+    let titles = [];
     // Loop through the objects in the input array
     // Add the value of title to the result array
     // Return the result array
