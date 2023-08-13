20d3c724cbf459faedfbba84b95ac90f8b94e9c9
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 15:56:28 -0700

    Return OOPS if negative

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,12 +1,17 @@
 const fibonacci = function(place) {
-    // Declare an array
-    let sequence = [1, 1];
-    // Add each fibonacci number to an array up to place - 1
-    for (i = 2; i < place; i++) {
-        sequence[i] = sequence[i - 2] + sequence[i - 1];
+    // Return "OOPS" if negative
+    if (place < 0) {
+        return "OOPS";
+    } else {
+        // Declare an array
+        let sequence = [1, 1];
+        // Add each fibonacci number to an array up to place - 1
+        for (i = 2; i < place; i++) {
+            sequence[i] = sequence[i - 2] + sequence[i - 1];
+        }
+        // Return array item at index[place - 1]
+        return sequence[place - 1];
     }
-    // Return array item at index[place - 1]
-    return sequence[place - 1];
 };
 
 // Do not edit below this line
