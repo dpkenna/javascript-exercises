9750ac54e2ea100beba3d92e811d52b3a801a237
Daniel Kenna <8608504+dpkenna@users.noreply.github.com>
2023-08-13 16:39:09 -0700

    Loop through the array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -2,11 +2,13 @@ const findTheOldest = function(array) {
     // Declare a result array
     let resultArray = [];
     // Loop through the array of objects
-    // If there is a death date
-        // Subtract the birth date from the death date
-    // Else
-        // Subtract the birth date from today's date
-    // Add the result to the result array
+    for (person in array) {
+        // If there is a death date
+            // Subtract the birth date from the death date
+        // Else
+            // Subtract the birth date from today's date
+        // Add the result to the result array
+    }
     // Compare the items in the result array
     // Return the index of the largest item
     // Return the object at the same index
